a34f04a50fab8c9df8b3929293300fb54721ed96
rizkyari <rizkyari4121@gmail.com>
2019-10-07 10:26:54 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,39 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet} from 'react-native';
+import { Container, Content, List, ListItem, Button, Text, Input, Item, Body } from 'native-base';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      datas: [
-        {id: 1, name:'work'},
-        {id: 2, name:'swim'},
-        {id: 3, name:'study'},
-        {id: 4, name:'sleep'},
-        {id: 5, name:'run'},
-      ]
-    };
+      lists: ['work','swim','study','sleep','run'],
+      data: ''
+    }// Array diubah karena id tidak diperlukan
+  }
+
+  show = () => {
+    return this.state.lists.map((list, index) => {
+      return (
+        <ListItem key={index}>
+          <Text>{list}</Text>
+        </ListItem>
+      );
+    });
+  }
+
+  onPress = () => {
+    const input = this.state.lists.concat(this.state.data);
+    this.setState({lists: input}, () => {
+      this.clear();
+    });
+  }
+
+  changeTextValue = (input) => {
+    this.setState({data: input});
+  }
+
+  clear = () => {
+    this.setState({data: ''});
   }
 
   render() {
     return (
-      <View style={styles.container}>
-      {this.state.datas.map((item, key) => {
-        return (
-          <Text style={styles.item} key={item.id}>
-            {item.name}
-          </Text>
-        );
-     })}
-     </View>
+      <Container>
+        <Content>
+          <Body style={styles.inputContent}>
+            <Item style={styles.input}>
+              <Input placeholder='Type Something'
+                onChangeText={text => {this.changeTextValue(text)}}
+                value={this.state.data} />
+            </Item>
+            <Button bordered onPress={this.onPress} style={styles.inputContent} >
+              <Text>Add</Text>
+            </Button>
+          </Body>
+          <List>
+            {this.show()}
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({  
-      item: {
-        borderBottomWidth: 0.5,
-        fontSize:23
-      }
-
-})
+const styles={
+    inputContent: {
+        flexDirection: 'row',
+    },
+    input: {
+        flex:8
+    },
+    body: {
+        flex: 2
+    }
+}
